d746d139e01a896e9525b074e3167c1e6e7e5ee8
Sergio De León <sergio.deleon@ubi-lab.com>
2024-01-22 22:10:26 +0900

    Testing button symbol: Use bigger icons

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
 console.log('Hello world!');
+
+/*
+  Buttons to include:
+  emoji_u1f3cb  Exercise
+  emoji_u1f46a  Being with others
+  emoji_u1f389  Party
+  emoji_u1f3ae  Playing vgames
+  emoji_u1f3e2  Working
+  emoji_u1f4fa  Watching a show
+  emoji_u1f622  Crying
+  emoji_u1f923  Having fun
+  emoji_u1f623  Anxious
+  emoji_u1f973  Aroused emotion
+  emoji_u1f37d  Eating
+  emoji_u1f6b6  Going out (walking)
+  emoji_u1f486  Relaxing
+*/
